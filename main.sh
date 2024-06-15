bcd8926c3b1c26ce2c2bf63d7c12d502e2e02e0b
shabane <m.mohamadshabane@gmail.com>
2024-06-16 03:25:06 +0330

    patch: change chatid

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	tar -c $BKP_DIR - | curl -F document=@- https://api.telegram.org/bot$TEL_TOKEN/sendDocument\?chat_id\=$CAHTID -v
+	tar -c $BKP_DIR - | curl -F document=@- https://api.telegram.org/bot$TEL_TOKEN/sendDocument\?chat_id\=$CHATID -v
 	sleep $SLEEP_TIME
 done
 
